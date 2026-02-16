ade3bee8fb8fc07042d6282b450f0bb54c90aee4
cytech <ken04150329@gmail.com>
2026-02-17 01:27:00 +0900

    initial Commit

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use\App\Models\Companies;
+use App\Models\Companies;
 use Illuminate\Http\Request;
 
 class CompaniesController extends Controller
--- app/Models/Sales.php
+++ app/Models/Sales.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 
 
 class Sales extends Model
